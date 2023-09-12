78689367dd973cac7c6142bcfe75361a059b7e32
Mahdi0024 <0024mahdi@gmail.com>
2023-09-12 22:43:51 +0330

    Add project files.

---
+++ SingBoxLib/Configuration/Log/LogConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace SingBoxLib.Configuration.Log;
+
+public sealed class LogConfig
+{
+    [JsonProperty("disabled")]
+    public bool? Disabled { get; set; }
+
+    [JsonProperty("level")]
+    public string? Level { get; set; }
+
+    [JsonProperty("output")]
+    public string? Output { get; set; }
+
+    [JsonProperty("timestamp")]
+    public bool? Timestamp { get; set; }
+}
