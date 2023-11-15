79853894f2819005da3c2c26801bf40ede6d59e3
Mahdi0024 <0024mahdi@gmail.com>
2023-11-15 23:41:10 +0330

    New and improved ParallelUrlTester implementaion

--- SingBoxLib/Runtime/Testing/ParallelUrlTester.cs
+++ SingBoxLib/Runtime/Testing/ParallelUrlTester.cs
@@ -1,40 +1,110 @@
-﻿using SingBoxLib.Parsing;
-using System.Collections.Concurrent;
+﻿using SingboxLib.Runtime.Api.Clash;
+using SingBoxLib.Configuration.Outbound.Abstract;
+using SingBoxLib.Configuration;
+using SingBoxLib.Parsing;
 
 namespace SingBoxLib.Runtime.Testing;
 
-public class ParallelUrlTester
+public class ParallelUrlTester : IDisposable
 {
-    private int _maxThreads;
-    private ConcurrentStack<UrlTester> _testers;
-
+    private readonly int _maxThreads;
+    private readonly int _localPort;
+    private readonly int _timeout;
+    private readonly string _testUrl;
+    private readonly ClashApiWrapper _clashApi;
+    private readonly SingBoxWrapper _singBoxWrapper;
     private int _activeThreads;
     public int ActiveThreads { get => _activeThreads; }
 
-    public ParallelUrlTester(SingBoxWrapper singBox, IEnumerable<int> localPorts, int maxThreads, int timeout, int attempts, string? url = null)
-    {
-        if (localPorts.Count() < maxThreads)
-        {
-            throw new ArgumentException($"Number of local ports count must be equal or higher than max threads.");
-        }
+    private bool _running;
 
+    public ParallelUrlTester(SingBoxWrapper singBoxWrapper, int localPort, int maxThreads, int timeout, string? testUrl = null)
+    {
+        _singBoxWrapper = singBoxWrapper;
         _maxThreads = maxThreads;
-        _testers = new ConcurrentStack<UrlTester>(localPorts
-            .Select(p => new UrlTester(singBox, p, timeout, attempts, url)));
+        _localPort = localPort;
+        _timeout = timeout;
+        _testUrl = testUrl ?? "http://cp.cloudflare.com/";
+        _clashApi = new ClashApiWrapper($"http://127.0.0.1:{_localPort}");
     }
 
     public async Task ParallelTestAsync(IEnumerable<ProfileItem> profiles, IProgress<UrlTestResult> progressReporter, CancellationToken cancellationToken)
     {
-        await Parallel.ForEachAsync(profiles, new ParallelOptions { MaxDegreeOfParallelism = _maxThreads }, async (profile, ct) =>
+        if (_running)
         {
-            Interlocked.Increment(ref _activeThreads);
-            _testers.TryPop(out var tester);
+            throw new InvalidOperationException($"Url tester is already running!");
+        }
+        _running = true;
 
-            var result = await tester!.TestAsync(profile);
-            progressReporter.Report(result);
 
-            _testers.Push(tester);
-            Interlocked.Decrement(ref _activeThreads);
-        });
+        var outbounds = new List<OutboundConfig>();
+        foreach (var profile in profiles)
+        {
+            var outbound = profile.ToOutboundConfig();
+            outbound.Tag = profile.Name;
+            outbounds.Add(outbound);
+        }
+
+
+        var config = new SingBoxConfig
+        {
+            Outbounds = outbounds,
+            Log = new()
+            {
+                Disabled = true,
+            },
+            Experimental = new()
+            {
+                ClashApi = new()
+                {
+                    ExternalController = $"127.0.0.1:{_localPort}",
+                }
+            }
+        };
+
+        using var cts = new CancellationTokenSource();
+        _ = _singBoxWrapper.StartAsync(config, cts.Token);
+        try
+        {
+            await Parallel.ForEachAsync(profiles, new ParallelOptions { MaxDegreeOfParallelism = _maxThreads }, async (profile, token) =>
+            {
+                Interlocked.Increment(ref _activeThreads);
+
+                using var requestTimeoutToken = new CancellationTokenSource(_timeout);
+                using var combinedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, requestTimeoutToken.Token);
+
+                var result = new UrlTestResult();
+                result.Profile = profile;
+
+                try
+                {
+                    var delayInfo = await _clashApi.GetProxyDelay(profile.Name!, _timeout, _testUrl, combinedCancellationToken.Token);
+                    result.Delay = delayInfo.Delay;
+                    result.Success = delayInfo.Success;
+                }
+                catch
+                {
+                    result.Success = false;
+                }
+                finally
+                {
+                    progressReporter.Report(result);
+                }
+                Interlocked.Decrement(ref _activeThreads);
+            });
+        }
+        finally
+        {
+            cts.Cancel();
+            _activeThreads = 0;
+            _running = false;
+        }
+
+    }
+
+    public void Dispose()
+    {
+        _clashApi.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
