e7fcba64bc370bead2f65309a4fc1e9e8ff737f5
Mahdi0024 <0024mahdi@gmail.com>
2023-12-01 19:05:31 +0330

    Add chunking support to ParallelUrlTester for better memmory efficiency

--- SingBoxLib/Runtime/Testing/ParallelUrlTester.cs
+++ SingBoxLib/Runtime/Testing/ParallelUrlTester.cs
@@ -2,6 +2,7 @@
 using SingBoxLib.Configuration.Outbound.Abstract;
 using SingBoxLib.Configuration;
 using SingBoxLib.Parsing;
+using SingBoxLib.Configuration.Log;
 
 namespace SingBoxLib.Runtime.Testing;
 
@@ -13,12 +14,13 @@ public class ParallelUrlTester : IDisposable
     private readonly string _testUrl;
     private readonly ClashApiWrapper _clashApi;
     private readonly SingBoxWrapper _singBoxWrapper;
+    private readonly int _testChunkCount;
     private int _activeThreads;
     public int ActiveThreads { get => _activeThreads; }
 
     private bool _running;
 
-    public ParallelUrlTester(SingBoxWrapper singBoxWrapper, int localPort, int maxThreads, int timeout, string? testUrl = null)
+    public ParallelUrlTester(SingBoxWrapper singBoxWrapper, int localPort, int maxThreads, int timeout, int testCunkCount, string? testUrl = null)
     {
         _singBoxWrapper = singBoxWrapper;
         _maxThreads = maxThreads;
@@ -26,6 +28,7 @@ public class ParallelUrlTester : IDisposable
         _timeout = timeout;
         _testUrl = testUrl ?? "http://cp.cloudflare.com/";
         _clashApi = new ClashApiWrapper($"http://127.0.0.1:{_localPort}");
+        _testChunkCount = testCunkCount;
     }
 
     public async Task ParallelTestAsync(IEnumerable<ProfileItem> profiles, IProgress<UrlTestResult> progressReporter, CancellationToken cancellationToken)
@@ -36,87 +39,95 @@ public class ParallelUrlTester : IDisposable
         }
         _running = true;
 
+        var testCunks = profiles.Chunk(_testChunkCount);
 
-        var outbounds = new List<OutboundConfig>();
-
-        var profileTagMap = new Dictionary<ProfileItem, int>();
-
-        var count = 0;
-        foreach (var profile in profiles)
+        foreach (var chunk in testCunks)
         {
-            try
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var outbounds = new List<OutboundConfig>();
+
+            var profileTagMap = new Dictionary<ProfileItem, int>();
+
+            var count = 0;
+            foreach (var profile in chunk)
             {
-                var outbound = profile.ToOutboundConfig();
-                outbound.Tag = count.ToString();
-                outbounds.Add(outbound);
-                profileTagMap.Add(profile, count);
-                count++;
-            }
-            catch
-            {
-                progressReporter.Report(new UrlTestResult
-                {
-                    Profile = profile,
-                    Success = false
-                });
-            }
-        }
-
-
-        var config = new SingBoxConfig
-        {
-            Outbounds = outbounds,
-            Log = new()
-            {
-                Disabled = true,
-            },
-            Experimental = new()
-            {
-                ClashApi = new()
-                {
-                    ExternalController = $"127.0.0.1:{_localPort}",
-                }
-            }
-        };
-
-        using var cts = new CancellationTokenSource();
-        _ = _singBoxWrapper.StartAsync(config, cts.Token);
-        try
-        {
-            await Parallel.ForEachAsync(profiles, new ParallelOptions { MaxDegreeOfParallelism = _maxThreads }, async (profile, token) =>
-            {
-                Interlocked.Increment(ref _activeThreads);
-
-                using var requestTimeoutToken = new CancellationTokenSource(_timeout);
-                using var combinedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, requestTimeoutToken.Token);
-
-                var result = new UrlTestResult();
-                result.Profile = profile;
-
                 try
                 {
-                    var delayInfo = await _clashApi.GetProxyDelay(profileTagMap[profile].ToString(), _timeout, _testUrl, combinedCancellationToken.Token);
-                    result.Delay = delayInfo.Delay;
-                    result.Success = delayInfo.Success;
+                    var outbound = profile.ToOutboundConfig();
+                    outbound.Tag = count.ToString();
+                    outbounds.Add(outbound);
+                    profileTagMap.Add(profile, count);
+                    count++;
                 }
                 catch
                 {
-                    result.Success = false;
+                    progressReporter.Report(new UrlTestResult
+                    {
+                        Profile = profile,
+                        Success = false
+                    });
                 }
-                finally
-                {
-                    progressReporter.Report(result);
-                }
-                Interlocked.Decrement(ref _activeThreads);
-            });
-        }
-        finally
-        {
-            cts.Cancel();
-            _activeThreads = 0;
-            _running = false;
-        }
+            }
 
+
+            var config = new SingBoxConfig
+            {
+                Outbounds = outbounds,
+                Log = new()
+                {
+                    Level = LogLevels.Error
+                },
+                Experimental = new()
+                {
+                    ClashApi = new()
+                    {
+                        ExternalController = $"127.0.0.1:{_localPort}",
+                    }
+                }
+            };
+
+            using var proccessCancellationToken = new CancellationTokenSource();
+            using var proccessAndInputCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(proccessCancellationToken.Token, cancellationToken);
+
+            try
+            {
+                var singBoxTask = _singBoxWrapper.StartAsync(config, proccessAndInputCancellationToken.Token);
+                await Parallel.ForEachAsync(profiles, new ParallelOptions { MaxDegreeOfParallelism = _maxThreads }, async (profile, token) =>
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    Interlocked.Increment(ref _activeThreads);
+
+                    using var requestTimeoutToken = new CancellationTokenSource(_timeout);
+                    using var combinedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(proccessAndInputCancellationToken.Token, requestTimeoutToken.Token);
+
+                    var result = new UrlTestResult();
+                    result.Profile = profile;
+
+                    try
+                    {
+                        var delayInfo = await _clashApi.GetProxyDelay(profileTagMap[profile].ToString(), _timeout, _testUrl, combinedCancellationToken.Token);
+                        result.Delay = delayInfo.Delay;
+                        result.Success = delayInfo.Success;
+                    }
+                    catch
+                    {
+                        result.Success = false;
+                    }
+                    finally
+                    {
+                        progressReporter.Report(result);
+                    }
+                    Interlocked.Decrement(ref _activeThreads);
+                });
+            }
+            finally
+            {
+                proccessCancellationToken.Cancel();
+            }
+        }
+        _running = false;
     }
 
     public void Dispose()
